01711d9723787604368cb87bc9eb0d31290f0bc6
Maksim_Alekseev <maxim040597@gmail.com>
2023-09-04 00:12:50 +0300

    добавление кнопки darkMod

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// кнопка подменю
-let btn = document.querySelectorAll('span.submenu');
 
-bth = addEventListener('click', (evt)=>{
-    // console.log(1);
-    evt.target.classList.toggle('show');
-})
+
+
