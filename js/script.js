a72824ff98d4c2585c3257949d8986c2f0a53ad4
laptop_Maksim_Alekseev <maxim040597@gmail.com>
2024-07-17 15:53:48 +0300

    создание нового Резюме

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 
+// Dark-mode
+let body =document.querySelector('html');
 
-
+if(!localStorage.getItem('screen')){
+    localStorage.setItem('screen', 'light');
+}
+if(localStorage.getItem('screen') == 'dark'){
+    body.classList.add('dark');
+}
+function onDark(){
+    switch(localStorage.getItem('screen')){
+        case 'light': 
+                body.classList.add('dark');
+                console.log('light = '+localStorage.getItem('screen'))
+                localStorage.setItem('screen', 'dark'); 
+                break;
+        case 'dark':
+                console.log('dark = '+localStorage.getItem('screen'))
+                body.classList.remove('dark'); 
+                localStorage.setItem('screen', 'light'); 
+                break;
+    }
+}
