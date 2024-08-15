f999fd633abe87bd1db96a30e56c3eb6b02d0411
Maksim_Alekseev <maxim040597@gmail.com>
2024-08-15 21:44:56 +0300

    js portfolio

--- js/script.js
+++ js/script.js
@@ -47,55 +47,92 @@
     const contents = 
     [
         {
+            id: 1,
             imgSmall:'site1.jpg',
             title:'Портфолио дизайнера',
             link: 'https://mrpepperoff.github.io/patent_Web/',
-            skill: '<ul><li><i class="fa-brands fa-html5"></i> HTML</li><li><i class="fa-brands fa-css3-alt"></i> Css</li><li><i class="fa-brands fa-sass"></i> Scss</li><li><i class="fa-brands fa-square-js"></i> JS</li></ul><p> также прочие Библиотеки</p>',
+            // skill: '<li><i class=""></i> HTML</li><li><i class="fa-brands fa-css3-alt"></i> Css</li><li><i class="fa-brands fa-sass"></i> Scss</li><li><i class="fa-brands fa-square-js"></i> JS</li></ul><p> также прочие Библиотеки</p>',
+            skills: [{awesome: 'fa-brands fa-html5', name: 'html'},{awesome: 'fa-brands fa-css3-alt', name: 'css'}],
             text: '<p>Это проект был создан, в рамках обучения в "Академии TOP".</p><p>Тут я использовал библиотеку "Bootstrap 5", "JQuery", так же использовал БЭМ-методологию</p> <p>Следует отметить, что базовый вектор развития создаёт предпосылки для соответствующих условий активизации. В своём стремлении улучшить пользовательский опыт мы упускаем, что интерактивные прототипы являются только методом политического участия и преданы социально-демократической анафеме. Современные технологии достигли такого уровня, что реализация намеченных плановых заданий позволяет оценить значение существующих финансовых и административных условий. Но глубокий уровень погружения играет определяющее значение для первоочередных требований. Идейные соображения высшего порядка, а также постоянный количественный рост и сфера нашей активности требует анализа распределения внутренних резервов и ресурсов. Следует отметить, что укрепление и развитие внутренней структуры требует от нас анализа форм воздействия. Разнообразный и богатый опыт говорит нам, что существующая теория однозначно фиксирует необходимость существующих финансовых и административных условий. Противоположная точка зрения подразумевает, что сделанные на базе интернет-аналитики выводы, вне зависимости от их уровня, должны быть преданы социально-демократической анафеме. Учитывая ключевые сценарии поведения, понимание сути ресурсосберегающих технологий позволяет оценить значение своевременного выполнения сверхзадачи. Как уже неоднократно упомянуто, некоторые особенности внутренней политики освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, своевременно верифицированы. Повседневная практика показывает, что понимание сути ресурсосберегающих технологий предполагает независимые способы реализации прогресса профессионального сообщества. Таким образом, современная методология разработки прекрасно подходит для реализации как самодостаточных, так и внешне зависимых концептуальных решений. Картельные сговоры не допускают ситуации, при которой ключевые особенности структуры проекта, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут функционально разнесены на независимые элементы. Банальные, но неопровержимые выводы, а также явные признаки победы институционализации, превозмогая сложившуюся непростую экономическую ситуацию, преданы социально-демократической анафеме. В частности, постоянный количественный рост и сфера нашей активности в значительной степени обусловливает важность благоприятных перспектив.</p>'
         },
         {
+            id: 2,
             imgSmall:'site2.jpg',
             title: "Магазин женской одежды",
             link: 'https://mrpepperoff.github.io/Divisima/',
-            skill: null,
+            skills: null,
             text: null
         },
         {
+            id: 3,
             imgSmall:'site3.jpg',
             title: 'Магазин спорт товаров',
             link: 'https://mrpepperoff.github.io/bicycle/',
-            skill: null,
+            skills: null,
             text: null
         },
         {
+            id: 4,
             imgSmall:'site4.jpg',
             title: 'Decor Elegant',
             link: 'https://decor-elegant.netlify.app/',
-            skill: '<ul><li><i class="fa-brands fa-react"></i> React 18</li><li><i class="fa-brands fa-laravel"></i> Laravel 10</li></ul>',
+            skills: null,
+            // skills: '<ul><li><i class="fa-brands fa-react"></i> React 18</li><li><i class="fa-brands fa-laravel"></i> Laravel 10</li></ul>',
             text: 'Этот сайт реализуется как свой пет-проект. Возможно в будущем будет работать. <br> К сожелению я не нашел хост для Back-end. <br> В этом проекте используются фреймворки: <br><br> front-end - "React 18" <br> back-end - "Laravel 10" <br><br> Конект осуществляется через Axios <br>В стадии реализации Админка на Laravel через ".blade" <br> Присутствует тема (light/dark), и мультиязычность (русский/англиский)'
         },
         {
+            id: 5,
             imgSmall:'site5.jpg',
-            title: 'Test test test test test test test test test test test tezt test ets test test',
+            title: 'Test',
             link: 'https://mrpepperoff.github.io/portfolio/',
-            skill: null,
+            skills: null,
             text: null
         },
     ]
-
+    const portfolio = document.querySelector('.portfolio__content');
+    contents.forEach(content => {
+        portfolio.innerHTML += 
+                `<div class="card">
+                    <div class="card__img__wrap">
+                        <img src="images/${content.imgSmall}" alt="site ${content.id}" class="card__img">
+                    </div>
+                    <h4>${content.title}</h4>
+                    <a href="${content.link}" class="card__link">Перейти</a>
+                    <button type="button" class="card__link" onclick="onModal(${content.id})">Подробнее</button>
+                </div>`
+    });
     function onModal(i){
         modal.classList.add('modal_active');
         const modalLeft = modal.querySelector('.modal__left');
         const modalRight = modal.querySelector('.modal__right');
+        i -= 1;
         modalLeft.innerHTML = '';
         modalRight.innerHTML = '';
 
 
-        modalRight.innerHTML += `<div class='modal__right-header'>${(contents[i].link == null)?`<button type='button' class="modal__link modal__link_disabled" disabled>Перейти на сайт</button>`:`<a href="${contents[i].link}" class="modal__link" >Перейти на сайт</a>`}<h4 class='modal__title'>${contents[i].title}</h4></div>`;
-        // modalRight.innerHTML += (contents[i].link == null)?`<button type='button' class="modal__link modal__link_disabled" disabled>Перейти на сайт</button>`:`<a href="${contents[i].link}" class="modal__link" >Перейти на сайт</a>`;
+        modalRight.innerHTML += 
+            `<div class='modal__right-header'>
+                ${(contents[i].link == null)?
+                    `<button type='button' class="modal__link modal__link_disabled" disabled>Перейти на сайт</button>`
+                    :
+                    `<a href="${contents[i].link}" class="modal__link" >Перейти на сайт</a>`
+                }
+                <h4 class='modal__title'>${contents[i].title}</h4>
+            </div>
+            <div class='modal__skill'>
+                <h5 class='modal__title-sub'>Использовались:</h5>
+                <ul>${
+                    contents[i].skills.forEach(skill => {
+                        console.log(skill.name);
+                        `<li>${skill.name}</li>`
+                    })
+                }</ul>
+            </div>
+            <div class='modal__text'>
+                <h5 class='modal__title-sub'>Описание:</h5>
+                <p> ${contents[i].text}</p>
+            </div>`;
 
-        modalRight.innerHTML += `<div class='modal__skill'><h5 class='modal__title-sub'>Использовались:</h5><p> ${contents[i].skill}</p></div>`;
-        modalRight.innerHTML += `<div class='modal__text'><h5 class='modal__title-sub'>Описание:</h5><p> ${contents[i].text}</p></div>`;
         
         if(contents[i].link != null){
             modalLeft.innerHTML = `<iframe src="${contents[i].link}" frameborder="0"></iframe>`
@@ -103,12 +140,13 @@
         else{
             modalLeft.innerHTML = `<img src="images/${contents[i].imgSmall}" alt="site ${i}" class="modal__img">`
         }
-        console.log(contents[i].modalLeft+' '+i);
     }
     function onModalClose(){
         modal.classList.remove('modal_active')
     }
+    function Portfolio(){
 
+    }
 
 // Иконкопереключатель
 const wrap = document.querySelector('#words');
